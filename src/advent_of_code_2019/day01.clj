d1685865459f11b9a408b4235f3ca030f2e8ce27
David M. Lee <leedm777@yahoo.com>
2019-11-30 23:57:36 -0600

    Use max instead of if

--- src/advent_of_code_2019/day01.clj
+++ src/advent_of_code_2019/day01.clj
@@ -3,15 +3,13 @@
 (defn calculate-fuel-naive
   [mass]
   (let [fuel   (- (int (/ mass 3)) 2)]
-    (if (> fuel 0)
-      fuel
-      0)))
+    (max fuel 0)))
 
 (defn calculate-fuel
   [mass]
   (if (> mass 0)
     (let [fuel (calculate-fuel-naive mass)
-          fuel-for-the-fuel (calculate-fuel fuel)]
+          fuel-for-the-fuel (calculate-fuel fuel)] ; who needs tail recursion?
       (+ fuel fuel-for-the-fuel))
     0))
 
