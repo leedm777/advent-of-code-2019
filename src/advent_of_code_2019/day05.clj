b4cf1a7228d6cb6a27f6789384f5b5244f46271f
David M. Lee <leedm777@yahoo.com>
2019-12-05 16:53:32 -0600

    Rewrote int-code to have more cohesive state
    Now hopefully additional features will just be new opcodes or new state.

--- src/advent_of_code_2019/day05.clj
+++ src/advent_of_code_2019/day05.clj
@@ -1,109 +1,119 @@
 (ns advent-of-code-2019.day05
   (:require [clojure.string :as s]))
 
-(defn read-mem
-  [program ip mode]
-  (let [val (program ip)]
-    (case mode
-      0 (program val)
-      1 val)))
+(defn initial-state
+  ([memory] (initial-state memory []))
+  ([memory input]
+   {:memory memory
+    :input input
+    :output []
+    :ip 0
+    :param-mode 0
+    :params []
+    :ptrs []
+    }))
+
+(defn read-instruction
+  "Read an opcode/param-mode instruction from memory"
+  [st]
+  (let [{:keys [memory ip]} st
+        instruction (memory ip)]
+    (merge st {:ip (inc ip)
+               :opcode (mod instruction 100)
+               :param-mode (quot instruction 100)
+               :ptrs []
+               :params []})))
+
+(defn read-param
+  "Read a parameter from memory"
+  [st]
+  (let [{:keys [memory ip ptrs params param-mode]} st
+        mode (mod param-mode 10)
+        ptr (case mode
+              ;; 0 - position mode
+              0 (memory ip)
+              ;; 1 - immediate mode
+              1 ip)
+        val (memory ptr)]
+    (merge st {:ip (inc ip)
+               :params (conj params val)
+               :ptrs (conj ptrs ptr)
+               :param-mode (quot param-mode 10)})))
+
+(defn read-params
+  "Read n parameters from memory"
+  [n st]
+  (nth (iterate read-param st) n))
 
 (def opcodes
-  {1 {;; addition
-      :func (fn [program input output modes ip]
-              (let [a (read-mem program (+ ip 1) (nth modes 0))
-                    b (read-mem program (+ ip 2) (nth modes 1))
-                    dest (program (+ ip 3))]
-                [(assoc program dest (+ a b))
-                 input
-                 output
-                 (+ ip 4)]))      }
-   2 {;; multiplication
-      :func (fn [program input output modes ip]
-              (let [a (read-mem program (+ ip 1) (nth modes 0))
-                    b (read-mem program (+ ip 2) (nth modes 1))
-                    dest (program (+ ip 3))]
-                [(assoc program dest (* a b))
-                 input
-                 output
-                 (+ ip 4)]))}
-   3 {;; input
-      :func (fn [program input output modes ip]
-              (let [dest (program (+ ip 1))]
-                [(assoc program
-                        dest
-                        (first input))
-                 (rest input)
-                 output
-                 (+ ip 2)]))}
-   4 {;; output
-      :func (fn [program input output modes ip]
-              (let [a (read-mem program (+ ip 1) (nth modes 0))]
-                [program
-                 input
-                 (conj output a)
-                 (+ ip 2)]))}
-   5 {;; jump-if-true
-      :func (fn [program input output modes ip]
-              (let [a (read-mem program (+ ip 1) (nth modes 0))
-                    target (read-mem program (+ ip 2) (nth modes 1))]
-                [program
-                 input
-                 output
-                 (if (zero? a)
-                   (+ ip 3)
-                   target)]))}
-   6 {;; jump-if-false
-      :func (fn [program input output modes ip]
-              (let [a (read-mem program (+ ip 1) (nth modes 0))
-                    target (read-mem program (+ ip 2) (nth modes 1))]
-                [program
-                 input
-                 output
-                 (if-not (zero? a)
-                   (+ ip 3)
-                   target)]))}
-   7 {;; less than
-      :func (fn [program input output modes ip]
-              (let [a (read-mem program (+ ip 1) (nth modes 0))
-                    b (read-mem program (+ ip 2) (nth modes 1))
-                    dest (program (+ ip 3))]
-                [(assoc program dest (if (< a b) 1 0))
-                 input
-                 output
-                 (+ ip 4)]))}
-   8 {;; equals
-      :func (fn [program input output modes ip]
-              (let [a (read-mem program (+ ip 1) (nth modes 0))
-                    b (read-mem program (+ ip 2) (nth modes 1))
-                    dest (program (+ ip 3))]
-                [(assoc program dest (if (= a b) 1 0))
-                 input
-                 output
-                 (+ ip 4)]))
-      }
-   })
-
-(defn param-mode
-  [digits]
-  (cons (mod digits 10) (lazy-seq (param-mode (quot digits 10)))))
+  {;; 1 - addition
+   1 (fn [st]
+       (let [st (read-params 3 st)
+             {:keys [params ptrs]} st
+             [a b] params
+             [_ _ dest] ptrs]
+         (assoc-in st [:memory dest] (+ a b))))
+   ;; 1 - multiplication
+   2 (fn [st]
+       (let [st (read-params 3 st)
+             {:keys [params ptrs]} st
+             [a b] params
+             [_ _ dest] ptrs]
+         (assoc-in st [:memory dest] (* a b))))
+   ;; 3 - input
+   3 (fn [st]
+       (let [st (read-param st)
+             {:keys [ptrs input memory]} st
+             [dest] ptrs
+             [val & input] (:input st)]
+         (merge st {:memory (assoc memory dest val)
+                    :input input})))
+   ;; 4 - output
+   4 (fn [st]
+       (let [st (read-param st)
+             {:keys [params output]} st
+             [val] params
+             output (conj output val)]
+         (merge st {:output output})))
+   ;; 5 - jump-if-true
+   5 (fn [st]
+       (let [st (read-params 2 st)
+             [a target] (:params st)]
+         (if (zero? a)
+           st
+           (assoc st :ip target))))
+   ;; 6 - jump-if-false
+   6 (fn [st]
+       (let [st (read-params 2 st)
+             [a target] (:params st)]
+         (if-not (zero? a)
+           st
+           (assoc st :ip target))))
+   ;; 7 - less than
+   7 (fn [st]
+       (let [st (read-params 3 st)
+             [a b] (:params st)
+             [_ _ dest] (:ptrs st)
+             val (if (< a b) 1 0)]
+         (assoc-in st [:memory dest] val)))
+   ;; 8 - equals
+   8 (fn [st]
+       (let [st (read-params 3 st)
+             [a b] (:params st)
+             [_ _ dest] (:ptrs st)
+             val (if (= a b) 1 0)]
+         (assoc-in st [:memory dest] val)))})
 
 (defn int-code
-  "Execute Intcode program"
-  ([p] (int-code p []))
-  ([p i]
-   (loop [program p
-          input i
-          output []
-          instruction-pointer 0]
-     (let [instruction (get program instruction-pointer)]
-       (if (= instruction 99)
-         { :mem program, :out output }
-         (let [opcode (mod instruction 100)
-               modes (param-mode (quot instruction 100))
-               {:keys [func]} (opcodes opcode)
-               [next-program next-input next-output next-ip] (func program input output modes instruction-pointer)]
-           (recur next-program next-input next-output next-ip)))))))
+  ([memory] (int-code memory []))
+  ([memory input]
+   (loop [st (initial-state memory input)]
+     (let [st (read-instruction st)
+           {:keys [opcode]} st]
+       (if (= opcode 99)
+         st
+         (do
+           (recur ((opcodes opcode) st))))))))
 
 (defn solve
   [input]
@@ -112,5 +122,5 @@
                   (s/split i #",")
                   (map #(Integer. %) i)
                   (vec i))]
-    {:part1 (:out (int-code program [1]))
-     :part2 (:out (int-code program [5]))}))
+    {:part1 (:output (int-code program [1]))
+     :part2 (:output (int-code program [5]))}))
