aeef272f928846c6692128c88b2fb4a76903caed
David M. Lee <leedm777@yahoo.com>
2019-12-10 11:09:48 -0600

    Refactor to include more info about other asteroids

--- src/advent_of_code_2019/day10.clj
+++ src/advent_of_code_2019/day10.clj
@@ -16,20 +16,36 @@
        (map-indexed (fn [idx row] (map vector row (repeat idx))))
        (apply concat)))
 
-(defn count-visible
+(defn relative-positions
   [asteroids [x y]]
   (->> asteroids
        (map (fn [[a-x a-y]]
-              (let [delta-y (- y a-y)
-                    delta-x (- x a-x)]
-                [(neg? delta-x)
-                 (neg? delta-y)
-                 (if (zero? delta-x)
-                   :undefined
-                   (/ delta-y delta-x))])))
+              (let [delta-x (- a-x x)
+                    delta-y (- a-y y)]
+                {:pos [a-x a-y]
+                 :delta-pos [delta-x delta-y]
+                 :quadrant (cond
+                             (and (>= delta-x 0) (>= delta-y 0)) 1
+                             (and (>= delta-x 0) (< delta-y 0)) 2
+                             (and (< delta-x 0) (< delta-y 0)) 3
+                             (and (< delta-x 0) (>= delta-y 0)) 4)
+                 :slope (cond
+                          (and (zero? delta-x) (zero? delta-y)) 0
+                          (and (zero? delta-x) (pos? delta-y)) Long/MAX_VALUE
+                          (and (zero? delta-x) (neg? delta-y)) Long/MIN_VALUE
+                          true (/ delta-y delta-x))
+                 :distance (Math/sqrt (+ (* delta-x delta-x) (* delta-y delta-y)))})))))
+
+
+(defn count-visible
+  [asteroids [x y]]
+  (->> (relative-positions asteroids [x y])
+       (filter #(not (= (:delta-pos %) [0 0])))
+       (map #(select-keys % [:slope :quadrant]))
        (set)
-       (count)
-       ))
+       ;;((fn [slopes] (println x y slopes) slopes))
+       (count)))
+
 
 (defn best-station
   [star-map]
@@ -37,6 +53,11 @@
         counter (map (fn [coords] [(count-visible asteroids coords) coords]) asteroids)
         best (apply max (map first counter))]
     (first (filter (fn [[x]] (= x best)) counter))))
+
+(defn kill-order
+  [star-map coords]
+  (let [asteroids (find-asteroids star-map)]
+    [1]))
 
 (defn solve
   [input]
