f880bdfe058e20aebb5503c7c5943536984bc847
David M. Lee <leedm777@yahoo.com>
2019-12-16 16:00:13 -0600

    Day 16 faster, but not fast enough

--- src/advent_of_code_2019/day16.clj
+++ src/advent_of_code_2019/day16.clj
@@ -1,44 +1,38 @@
 (ns advent-of-code-2019.day16
   (:require [clojure.string :as s]))
 
-(def base-pattern
-  (memoize
-   (fn [n]
-     (->> [0 1 0 -1]
-          ;; repeat each digit n times
-          (map #(repeat n %))
-          ;; repeat those repeated digits infinitely
-          (repeat)
-          ;; flatten the list
-          (flatten)
-          ;; skip the first one
-          (drop 1)))))
-
 (defn parse-signal
   [input]
   (->> input
        (s/trim)
        (map #(- (int %) (int \0)))))
 
-(defn tap
-  [n]
-  (println n)
-  n)
-
 (defn last-digit
   [n]
   (Math/abs (rem n 10)))
+
+(defn fft-i
+  [signal i]
+  ;; skip the initial (dec i) values
+  ;; for the next 4i values, we will
+  ;;  - add the first partition
+  ;;  - skip the second
+  ;;  - sub the third
+  ;;  - skip the fourth
+  (let [signal (drop (dec i) signal)
+        ranges (partition i (+ i i) [] signal)
+        add-subs (partition 2 2 []  ranges)]
+    (last-digit (reduce (fn [acc [add sub]]
+                          (apply - (apply + acc add) sub))
+                        0
+                        add-subs))))
 
 (defn fft
   [signal]
   (let [signal-length (count signal)
         iter (range 1 (inc signal-length))]
     (->> iter
-         (mapv (fn [i]
-                 (->> (base-pattern i)
-                      (mapv * signal)
-                      (apply +)
-                      (last-digit)))))))
+         (mapv (partial fft-i signal)))))
 
 (defn long-fft
   [signal]
@@ -48,8 +42,8 @@
                   (s/join)
                   (Long/parseLong))
         final-phase (nth (iterate fft long-signal) 100)
-        message (subvec final-phase addr (+ addr 8))
-        ]))
+        message (subvec final-phase addr (+ addr 8))]
+    message))
 
 (defn solve
   [input]
