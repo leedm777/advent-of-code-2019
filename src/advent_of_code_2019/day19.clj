7775ca02742c7b6d2b5212af15d359f4507e9484
David M. Lee <leedm777@yahoo.com>
2019-12-19 00:00:00 -0600

    Day 19 idea

--- src/advent_of_code_2019/day19.clj
+++ src/advent_of_code_2019/day19.clj
@@ -24,6 +24,22 @@
        (map second)
        (reduce +)))
 
+(defn fits-sleigh?
+  [drone pos]
+  ;; x  - pos has pull
+  ;; r1 - (move pos [99 0]) has pull
+  ;; d1 - (move post [0 99]) has pull
+  ;; r2 - (move pos [100 0]) has no pull
+  ;; d2 - (move post [0 100]) has no pull
+  ;; slope of beam is roughly [5 4]
+
+
+  ;; find spot where x, r2, d2 all have pull
+  ;; move left until d1/d2 are good
+  ;; move up until r1/r2 are good
+  ;; repeat moving left and up until all points are good
+  )
+
 (defn solve
   [input]
   (let [program (int-parse input)
