6a0853503d0b043d42b59b8ec3ee967eb02186e7
David M. Lee <leedm777@yahoo.com>
2019-12-29 00:54:49 -0600

    Day 19, part 2
    I got to the right answer, but feels a bit like cheating.
    I discovered a good point to start searching for through trial
    and error, instead of just letting the program find it.

--- src/advent_of_code_2019/day19.clj
+++ src/advent_of_code_2019/day19.clj
@@ -14,9 +14,10 @@
     (= 1 pulled)))
 
 (defn full-map
-  [drone]
-  (for [pos full-range]
-    [pos (has-pull? drone pos)]))
+  ([drone] (full-map drone full-range))
+  ([drone map-range]
+   (for [pos map-range]
+     [pos (has-pull? drone pos)])))
 
 (defn count-pulls
   [m]
@@ -38,17 +39,24 @@
        (not (has-pull? drone (pos-move pos [100 0])))
        (not (has-pull? drone (pos-move pos [0 100])))))
 
+;; roughly follows the slope of the beam
+(def beam-slope-delta [49 40])
+
 (defn find-rough-fit
   ([drone]
-   ;;(find-rough-fit drone [49 40])
-   [1319 1058])
+   ;;(find-rough-fit drone beam-slope-delta)
+   ;; trial and error got that far
+   [1300 1040]
+   )
   ([drone pos]
    (if-not (has-pull? drone pos)
      :lost
      (if (and (has-pull? drone (pos-move pos [99 0]))
               (has-pull? drone (pos-move pos [0 99])))
-       pos
-       (recur drone (pos-move pos [49 40]))))))
+       ;; move back to the prior pos
+       (pos-move pos (mapv #(* -1 %) beam-slope-delta))
+       ;; try further down the slope
+       (recur drone (pos-move pos beam-slope-delta))))))
 
 ;; slope of beam is roughly [5 4]
 ;; find spot where x, r2, d2 all have pull
@@ -59,34 +67,35 @@
   [drone]
   (let [pos (find-rough-fit drone)]
     (loop [pos pos]
-      (println pos)
-      (println "  " (has-pull? drone pos))
-      (println "  " (has-pull? drone (pos-move pos [100 0])))
-      (println "  " (has-pull? drone (pos-move pos [0 100])))
-      (println "  " (has-pull? drone (pos-move pos [99 0])))
-      (println "  " (has-pull? drone (pos-move pos [0 99])))
+      ;;(println pos)
+      ;;(println "  " (has-pull? drone pos))
+      ;;(println "  " (has-pull? drone (pos-move pos [99 0])))
+      ;;(println "  " (has-pull? drone (pos-move pos [0 99])))
+      ;;(println "  " (has-pull? drone (pos-move pos [100 0])))
+      ;;(println "  " (has-pull? drone (pos-move pos [0 100])))
       (cond
         (not (has-pull? drone pos)) :lost
+        ;; move down
+        (not (has-pull? drone (pos-move pos [99 0]))) (recur (pos-move pos [0 1]))
+        ;; move right
+        (not (has-pull? drone (pos-move pos [0 99]))) (recur (pos-move pos [1 0]))
         ;; move up
         (has-pull? drone (pos-move pos [100 0])) (recur (pos-move pos [0 -1]))
         ;; move left
         (has-pull? drone (pos-move pos [0 100])) (recur (pos-move pos [-1 0]))
-        ;; move down
-        (not (has-pull? drone (pos-move pos [99 0]))) (recur (pos-move pos [0 1]))
-        ;; move up
-        (not (has-pull? drone (pos-move pos [0 99]))) (recur (pos-move pos [1 0]))
         :else pos))))
 
 (defn solve
   [input]
   (let [program (int-parse input)
         drone (int-code program)
-        ;;m (full-map drone)
+        m (full-map drone)
         ]
     ;;(println (render-map (apply conj {} m) #(case %
-    ;;                                          0 \.
-    ;;                                          1 \#)))
-    {
-     ;;:full-map    (count-pulls m)
+    ;;                                          false \.
+    ;;                                          true \#)))
+    {:full-map    (count-pulls m)
      :find-sleigh (let [[x y] (find-sleigh drone)]
                     (+ y (* 10000 x)))}))
+
+;; 13081049 is correct
